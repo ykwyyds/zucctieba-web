a689d202ff2b79b432029609c407abab2ad4ecdc
yukaiwen <1342781649@qq.com>
2023-04-01 21:33:13 +0800

    首页初步布局，帖子模块ing  CJNB！

---
+++ src/views/HomePage/HomePage.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="search-bar">
+      <input type="text" v-model="searchTerm" placeholder="搜索...">
+      <button @click="search">搜索</button>
+    </div>
+    <div class="container">
+      <div class="hotspot" :style="divStyle">
+        <h2>热点1</h2>
+        <p>这里是热点1的内容</p>
+      </div>
+      <div :style="divStyle" class="hotspot">
+        <h2>热点2</h2>
+        <p>这里是热点2的内容</p>
+      </div>
+      <div :style="divStyle" class="hotspot">
+        <h2>热点3</h2>
+        <p>这里是热点3的内容</p>
+      </div>
+      <div :style="divStyle" class="hotspot">
+        <h2>热点4</h2>
+        <p>这里是热点4的内容</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HomePage',
+  data() {
+    return {
+      searchTerm: '',  //可以diy搜索框内的内容，双向绑定
+      divStyle:{
+    height: '500px'  //四个区域的高度
+      }
+    };
+  },
+  methods: {
+      mounted(){
+
+      },
+    search() {
+      console.log(this.searchTerm)
+      // 这里编写搜索功能的代码
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-gap: 20px;
+
+}
+
+.hotspot {
+  background-color: #ccc;
+  border: 1px solid #000;
+  text-align: center;
+
+}
+.search-bar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.search-bar input {
+  width: 300px;
+  height: 30px;
+  padding: 5px;
+  margin-right: 10px;
+}
+
+.search-bar button {
+  height: 40px;
+  padding: 5px 10px;
+  background-color: #42b983;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+
+</style>
---
+++ src/views/TieBa/TieBa.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="post">
+    <div class="post-image">
+      <!-- 发帖图片 -->
+      <img :src="post.imageUrl" alt="Post Image" v-bind:style="{ width: '50px', height: '50px' }">
+    </div>
+    <div class="post-info">
+      <!-- 发帖信息 -->
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.description }}</p>
+      <div class="post-actions">
+        <!-- 点赞按钮 -->
+        <button @click="likePost">
+          {{ post.likes }} {{ post.likes === 1 ? 'like' : 'likes' }}
+        </button>
+        <!-- 收藏按钮 -->
+        <button @click="favoritePost">
+          {{ post.favorites }} {{ post.favorites === 1 ? 'favorite' : 'favorites' }}
+        </button>
+        <!-- 分享按钮 -->
+        <button @click="sharePost">
+          Share
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TieBa',
+  data() {
+    return {
+      post: {
+        title: 'My Post',
+        description: 'This is a description of my post.',
+        imageUrl: 'http://h.hiphotos.baidu.com/image/pic/item/7c1ed21b0ef41bd5f2c2a9e953da81cb39db3d1d.jpg',
+        likes: 0,
+        favorites: 0
+      }
+    }
+  },
+  methods: {
+    likePost() {
+      this.post.likes++
+    },
+    favoritePost() {
+      this.post.favorites++
+    },
+    sharePost() {
+      // 实现分享功能
+    }
+  }
+}
+</script>
+
+<style>
+.post {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.post-image img {
+  max-width: 100%;
+}
+
+.post-info {
+  margin: 20px;
+}
+
+.post-actions button {
+  margin-right: 10px;
+}
+</style>
