152cb27a5eb5ca1004eb4f5272eb70ca0995ee40
yukaiwen <1342781649@qq.com>
2023-04-01 22:55:01 +0800

    点赞按钮实现

--- src/views/TieBa/TieBa.vue
+++ src/views/TieBa/TieBa.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="post">
-    <div class="post-image">
-      <!-- 发帖图片 -->
-      <img :src="post.imageUrl" alt="Post Image" v-bind:style="{ width: '50px', height: '50px' }">
-    </div>
-    <div class="post-info">
-      <!-- 发帖信息 -->
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.description }}</p>
-      <div class="post-actions">
-        <!-- 点赞按钮 -->
-        <button @click="likePost">
-          {{ post.likes }} {{ post.likes === 1 ? 'like' : 'likes' }}
-        </button>
-        <!-- 收藏按钮 -->
-        <button @click="favoritePost">
-          {{ post.favorites }} {{ post.favorites === 1 ? 'favorite' : 'favorites' }}
-        </button>
-        <!-- 分享按钮 -->
-        <button @click="sharePost">
-          Share
-        </button>
+    <div class="post-header">
+<!--      <img class="avatar" v-bind:src="user.avatar" alt="User Avatar" />-->
+      <div class="user-info">
+        <h2>{{ user.name }}</h2>
+        <p class="username">@{{ user.username }}</p>
       </div>
+    </div>
+    <div class="post-content">
+      <p>{{ content }}</p>//
+      <img v-if="image" class="post-image" v-bind:src="image" alt="Post Image" v-bind:style="{ width: '50px', height: '50px' }" />
+    </div>
+    <div class="post-footer">
+      <div class="likes">
+<!--        <i class="fa fa-heart"></i>-->
+<!--        <span>{{ likes }}</span>-->
+        <button @click="toggleLike" :class="{ liked: liked }">
+          <img src="@/assets/images/like.png" v-if="liked" />
+          <img src="@/assets/images/unlike.png" v-else />
+          </button>
+      </div>
+<!--      <div class="comments">-->
+<!--        <i class="fa fa-comment"></i>-->
+<!--        <span>{{ comments }}</span>-->
+<!--      </div>-->
+<!--      <div class="date">-->
+<!--        <span>{{ date }}</span>-->
+<!--      </div>-->
     </div>
   </div>
 </template>
@@ -29,48 +34,143 @@
 <script>
 export default {
   name:'TieBa',
-  data() {
-    return {
-      post: {
-        title: 'My Post',
-        description: 'This is a description of my post.',
-        imageUrl: 'http://h.hiphotos.baidu.com/image/pic/item/7c1ed21b0ef41bd5f2c2a9e953da81cb39db3d1d.jpg',
-        likes: 0,
-        favorites: 0
+  props: {
+    // user: {
+    //   type: Object,
+    //   required: true
+    // },
+    // content: {
+    //   type: String,
+    //   required: true
+    // },
+    // image: {
+    //   type: String,
+    //   default: null
+    // },
+    // likes: {
+    //   type: Number,
+    //   default: 0
+    // },
+    // comments: {
+    //   type: Number,
+    //   default: 0
+    // },
+    // date: {
+    //   type: String,
+    //   required: true
+    // }
+  },
+  data(){
+    return{
+      // eslint-disable-next-line vue/no-dupe-keys
+      user: {
+        name:'s',
+        username:'s'
       }
+      ,content: 'xxxxxxxxxxxxx',
+      image:'http://h.hiphotos.baidu.com/image/pic/item/7c1ed21b0ef41bd5f2c2a9e953da81cb39db3d1d.jpg',
+      likes:'1',
+      liked: false, // 初始状态为未点赞
     }
   },
   methods: {
-    likePost() {
-      this.post.likes++
+    toggleLike() {
+      // 点赞操作
+      this.liked = !this.liked; // 点赞状态取反
     },
-    favoritePost() {
-      this.post.favorites++
-    },
-    sharePost() {
-      // 实现分享功能
-    }
-  }
+  },
 }
 </script>
 
 <style>
 .post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px 0;
+}
+
+.post-header {
   display: flex;
   align-items: center;
-  justify-content: center;
-  flex-direction: column;
 }
 
-.post-image img {
-  max-width: 100%;
-}
-
-.post-info {
-  margin: 20px;
-}
-
-.post-actions button {
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
   margin-right: 10px;
+}
+
+.user-info h2 {
+  font-size: 18px;
+  margin: 0;
+}
+
+.username {
+  color: #aaa;
+  margin: 0;
+}
+
+.post-content {
+  margin: 10px 0;
+}
+
+.post-content p {
+  margin: 0;
+}
+
+.post-image {
+  max-width: 100%;
+  margin-top: 10px;
+}
+
+.post-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.likes, .comments, .date {
+  display: flex;
+  align-items: center;
+}
+
+.likes i, .comments i {
+  margin-right: 5px;
+}
+
+.date span {
+  color: #aaa;
+}
+.like-container {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  display: flex;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  padding: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+}
+
+.like-container i {
+  color: #3b5998;
+  font-size: 20px;
+  margin-right: 5px;
+}
+
+.like-container .like-count {
+  font-size: 16px;
+  font-weight: bold;
+  color: #333;
+}
+button.liked {
+  /*background-color: #ffdd00; !* 将点赞按钮的背景颜色改为黄色 *!*/
+}
+button img {
+  width: 24px;
+  height: 24px;
 }
 </style>
